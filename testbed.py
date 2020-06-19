ce6f0e2cf05a59ca47995b3d4736c45daf5a5c6e
OneTwoThree <paulpc.dev@gmail.com>
2020-06-19 01:16:33 -0700

    Update testbed.py

--- testbed.py
+++ testbed.py
@@ -1,7 +1,7 @@
 import m_ssh
 import re
 
-cmd_run = "cat /var/log/syslog | grep DHCPACK | grep 08:00:27:04:f6:6d | tail -1"
+cmd_run = "cat /var/log/syslog | grep DHCPACK | grep 07:00:27:04:f6:6d | tail -1"
 result = m_ssh.run("10.5.5.1", cmd_run, "testbed1", "testbed1")
 if result:
     foobar = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', result[0])
@@ -11,7 +11,7 @@ if foobar:
     serv_ip = foobar.group(0)
 
 print serv_ip
-result = m_ssh.run(serv_ip, "ps -ef", "root", "nbv12345")
+result = m_ssh.run(serv_ip, "ps -ef", "root", "pa$$w0ord")
 
 #print result
 
